b1e4754940ec180e2293990756530702ed22e619
Susan Rehl <susanrehl@gmail.com>
2016-07-23 16:53:30 -0500

    final project

---
+++ README.md
@@ -0,0 +1,1 @@
+Pet images and data viewer using MEAN stack (MongoDB, AngularJS, Express, NodeJS). Users can insert pet images and basic data, delete pets, and view pets.
--- server/app.js
+++ server/app.js
@@ -13,34 +13,22 @@ app.get( '/', function( req, res ){    // set basic url
   res.sendFile( path.resolve( 'views/index.html' ) );
 });
 
-app.get( '/view', function( req, res ){  // GET function to retrieve data
+app.get( '/view', function( req, res ){  // makes view.html available
   res.sendFile( path.resolve( 'views/view.html' ) );
   });
 
-app.get('/viewPets', function(req, res){
+app.get('/viewPets', function(req, res){  // gets pets to view
   nupets.find().then( function( data ){  // MAGIC! - all new and existing are found here// similar to ajax get call - if found, then run function with data as parameter
       // console.log("data from app" + data);
     res.send( data );  // returns records as "data"
   });
 });
 
-app.get( '/add', function( req, res ){  // GET function to retrieve data
+app.get( '/add', function( req, res ){  // makes add.html page available
   res.sendFile( path.resolve( 'views/add.html' ) );
   });
 
-app.get( '/addPet', function( req, res ){  // GET function to retrieve data
-  nupets.find().then( function( data ){  // MAGIC! - all new and existing are found here// similar to ajax get call - if found, then run function with data as parameter
-    // console.log("data from app" + data);
-    res.send( data );  // returns records as "data"
-  });
-}); //end get
-
-
-app.listen( 8080, 'localhost', function( req, res ){ // spins up server
-  console.log( 'listening on 8080' );
-}); // end listen
-
-app.post( '/add', function( req, res ){  // POST call
+app.post( '/add', function( req, res ){  // POST call to add pet to database
   var recordToAdd={  // adds record from input
     name: req.body.name,
     animal: req.body.animal,
@@ -51,30 +39,6 @@ app.post( '/add', function( req, res ){  // POST call
   newRecord.save();
   console.log("new record from app.post: " + newRecord);
 });  // end post
-
-
-//Lukas' code
-app.post('/newPet', function(req, res){
-	var newPet = new Pets( {
-	  name: req.body.name,
- 	 animal_type: req.body.type,
-	  age_years: req.body.age,
-	  img_url: req.body.img_url
-	});
-  	newPet.save(function(err){
- 	   if(err) {
- 	     console.log(err);
- 	   } else {
-  	    console.log('Pet saved successfully!');
- 	     res.sendStatus(200);
-	    }
-	  });
-});
-
-
-
-
-
 
 app.post('/deletePet', function(req, res) {  // delete pet
   console.log(req.body.id);
@@ -91,5 +55,9 @@ app.post('/deletePet', function(req, res) {  // delete pet
 }
 });
 }); //end delete function
+
+app.listen( 8080, 'localhost', function( req, res ){ // spins up server
+  console.log( 'listening on 8080' );
+}); // end listen
 
 app.use( express.static( 'public' ) );  // make public folder available
